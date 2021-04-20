8d79232a9dc0b1f5349c5d32909fe32504bab657
InadilsonJr <73889359+InadilsonJr@users.noreply.github.com>
2021-04-20 11:03:46 +0100

    Set up HomeScreen
    I set up HomeScreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,41 +1,51 @@
-
-
 import React, { useEffect } from 'react';
-import { View, Text, SafeAreaView } from 'react-native';
+import { StyleSheet, View, Text, SafeAreaView } from 'react-native';
 import Mybutton from './Components/MyButtons';
 import Mytext from './Components/Mytext';
 import * as SQLite from 'expo-sqlite';
 
-const db = SQLite.openDatabase('UserDatabase.db');
-
-
-
+const db = SQLite.openDatabase('employees.db');
 const HomeScreen = ({ navigation }) => {
   useEffect(() => {
     db.transaction(function (txn) {
       txn.executeSql(
-        "SELECT name FROM sqlite_master WHERE type='table' AND name='table_user'",
+        "SELECT name FROM sqlite_master WHERE type='table' AND name='employees'",
         [],
         function (tx, res) {
           console.log('item:', res.rows.length);
           if (res.rows.length == 0) {
-            txn.executeSql('DROP TABLE IF EXISTS table_user', []);
+            txn.executeSql('DROP TABLE IF EXISTS employees', []);
             txn.executeSql(
-              'CREATE TABLE IF NOT EXISTS table_user(user_id INTEGER PRIMARY KEY AUTOINCREMENT, user_name VARCHAR(20), user_contact INT(10), user_address VARCHAR(255))',
-              []
-            );
+              `CREATE TABLE IF NOT EXISTS employees
+                  (
+                      id INTEGER PRIMARY KEY NOT NULL,
+                      firstName TEXT NOT NULL,
+                      lastName TEXT NOT NULL,
+                      gender TEXT NOT NULL,
+                      department TEXT NOT NULL                
+                  );
+              `,[]);
           }
         }
       );
     });
   }, []);
 
+  const styles = StyleSheet.create({
+ 
+    btn: {
+      height: 20,
+ 
+    },
+  });
+ 
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <View style={{ flex: 1, backgroundColor: 'white' }}>
-        <View style={{ flex: 1 }}>
+    <SafeAreaView style={{  }}>
+      <View style={{backgroundColor: 'white' }}>
+        <View style={{}}>
           <Mytext text="SQLite Example" />
           <Mybutton
+            style={styles.btn}
             title="Register"
             customClick={() => navigation.navigate('Register')}
           />
@@ -48,33 +58,16 @@ const HomeScreen = ({ navigation }) => {
             customClick={() => navigation.navigate('View')}
           />
           <Mybutton
-            title="View All"
-            customClick={() => navigation.navigate('ViewAll')}
-          />
-          <Mybutton
             title="Delete"
             customClick={() => navigation.navigate('Delete')}
           />
         </View>
-        <Text
-          style={{
-            fontSize: 18,
-            textAlign: 'center',
-            color: 'grey'
-          }}>
-          Example of SQLite Database in React Native
-        </Text>
-        <Text
-          style={{
-            fontSize: 16,
-            textAlign: 'center',
-            color: 'grey'
-          }}>
-          www.aboutreact.com
-        </Text>
-      </View>
+       </View>
     </SafeAreaView>
   );
+
 };
+
+
 
 export default HomeScreen;
